8baf8eef71b36e7a535cd5d46d4d449e552de263
Alexandru Radovici <msg4alex@gmail.com>
2016-03-10 18:12:20 +0200

    Update arduinoyun_install.sh

--- scripts/arduinoyun_install.sh
+++ scripts/arduinoyun_install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 opkg update
-opkg install git make gcc node-serialport node-tty.js avr-toolchain arduino-files -d mnt
+opkg install git make node-serialport node-tty.js avr-toolchain arduino-files -d mnt
 
 cp -R /opt/usr/lib/node_modules/tty.js/node_modules/pty.js /opt/usr/lib/node_modules
 
