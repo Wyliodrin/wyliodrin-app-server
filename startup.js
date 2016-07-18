4e90e70d4f7fec4a3f894770e0c80c83cb1ecff9
raz <serban-razvan@github.com>
2016-07-18 16:27:33 +0300

    download works

--- startup.js
+++ startup.js
@@ -530,10 +530,9 @@ function linux_ls(place,list)
 		        var lss = {};
 		        lss["name"] = file;
 		        lss["size"] = fs.statSync(path.join(place,file))["size"];
-		        lss["isdir"] = fs.lstatSync(path.join(place,file)).isDirectory()
-		        lss["isfile"] = fs.lstatSync(path.join(place,file)).isFile()
-		        lss["islink"] = fs.lstatSync(path.join(place,file)).isSymbolicLink()
-		        console.log(lss);
+		        lss["isdir"] = fs.lstatSync(path.join(place,file)).isDirectory();
+		        lss["isfile"] = fs.lstatSync(path.join(place,file)).isFile();
+		        lss["islink"] = fs.lstatSync(path.join(place,file)).isSymbolicLink();
 		        ls.push(lss);
 			});
 	    	list(ls);
