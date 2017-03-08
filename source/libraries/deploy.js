44c8e98f4b7b7bb4803bd375473ad26e56dc0d60
Serban Razvan <serban.razvan.96@gmail.com>
2017-03-08 13:43:02 +0200

    WIP

--- source/libraries/deploy.js
+++ source/libraries/deploy.js
@@ -6,6 +6,7 @@ var debug = require ('debug')('wylidorin:app:server:file_explorer');
 var uplink = require ('./uplink');
 var rmdir = require ("rimraf");
 var settings = require("./settings");
+var spawn = require("child_process").spawnSync
 
 
 console.log ('Loading deploy library');
@@ -14,24 +15,37 @@ console.log ('Loading deploy library');
 var DIR = path.join(path.dirname(settings.SETTINGS.build_file),'deploy');
 var INFO_FILE = "info.json";
 var SUPERVISOR_DIR = "/etc/supervisor/conf.d";
+var SUPERVISOR_PREFIX = "wyliodrin.";
 
 debug ('Registering for tag dep');
 uplink.tags.on ('dep', function (p)
 {
 	if (p.a == "ls")
 	{
-		projects = fs.readdirSync(srcpath).filter(file => fs.statSync(path.join(srcpath, file)).isDirectory());
+		var projects = fs.readdirSync(DIR).filter(function (value){
+			return !value.startsWith("wyliodrin-");
+		});
+		//pune busy status  (status)hash si tot ce e in info.json
 	}
 	if (p.a == "stop")
 	{
+		var hash = p.b;
+    	var cmd = spawn("sudo supervisorctl stop " + SUPERVISOR_PREFIX + hash);
+    	uplink.send ('dep', {a:"ACK", b:obj.hash});
 		///////////////////////fa stop pe hashul p.b, da ack
 	}
 	if (p.a == "run")
 	{
+		var hash = p.b;
+    	var cmd = spawn("sudo supervisorctl start " + SUPERVISOR_PREFIX + hash);
+    	uplink.send ('dep', {a:"ACK", b:obj.hash});
 		////////////////////////fa run pe hashul p.b da ack
 	}
 	if (p.a == "restart")
 	{
+		var hash = p.b;
+    	var cmd = spawn("sudo supervisorctl restart " + SUPERVISOR_PREFIX + hash);
+    	uplink.send ('dep', {a:"ACK", b:obj.hash});
 		////////////////////////fa restart pe hashul p.b da ack
 	}
 	if (p.a == "deploy")
@@ -54,7 +68,7 @@ uplink.tags.on ('dep', function (p)
 		fs.writeFileSync(path.join(local,main.name), main.content);
 
 		//make supervisord file
-		fs.writeFileSync(path.join(SUPERVISOR_DIR,obj.supervisor_name), obj.supervisor_file);
+		fs.writeFileSync(path.join(SUPERVISOR_DIR, SUPERVISOR_PREFIX + obj.hash), obj.supervisor_file);
 
 		//ack
 		uplink.send ('dep', {a:"ACK", b:obj.hash});
@@ -64,7 +78,8 @@ uplink.tags.on ('dep', function (p)
 		var hash = p.b;
 		var local = path.join(DIR,hash);
 		//////////////////////////////////////////////////////////////////////////////STOP THE SERVICE
-		rimraf.sync(local);
+		rimraf.sync(local); //deleted folder
+		rimraf.sync(path.join(SUPERVISOR_DIR, "wyliodrin." + hash)); //deleted supervisord script
 	}
 	if (p.a == "redeploy")
 	{
