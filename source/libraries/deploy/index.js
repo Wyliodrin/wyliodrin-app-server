572c74c15c5ebc343a0b0e3f18532a2788d864cd
CatalinDabuleanu <catalindabuleanu@gmail.com>
2017-12-08 14:07:45 +0200

    sth

--- source/libraries/deploy/index.js
+++ source/libraries/deploy/index.js
@@ -474,6 +474,7 @@ uplink.tags.on ('dep', function (p)
 		console.log(errlog);
 		var cmdtemp = "sudo cat "+errlog+" > /home/pi/"+nameoferrlog;
 		exec(cmdtemp);
+		console.log(tempfile);
 		var tempfile="/home/pi/"+nameoferrlog;
 		uplink.send('dep',{a:'errlogpath',b:nameoferrlog});
 		send_file(tempfile,index,MAXPACKET,
